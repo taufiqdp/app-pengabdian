18451e09d7602a4fcf3c675f70fe2cd0cb847424
taufiqdp <topexdw@gmail.com>
2024-07-26 16:57:57 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
