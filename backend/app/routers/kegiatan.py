257c71ed2116ff6a38deece689e97c9f0e0062be
taufiqdp <topexdw@gmail.com>
2024-09-04 08:59:20 +0700

    update: add upload image, get kegiatan by id, and update kegiatan

--- backend/app/routers/kegiatan.py
+++ backend/app/routers/kegiatan.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
-from fastapi import APIRouter, status, HTTPException
+from pydantic import BaseModel, Json
+from fastapi import APIRouter, status, HTTPException, UploadFile, File
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 
 from app.models import Kegiatan
 from app.dependencies import db_dependency, user_dependency
@@ -14,22 +14,20 @@ class KegiatanCreateRequest(BaseModel):
     tanggal: datetime
     tempat: str
     deskripsi: Optional[str] = None
-    gambar: Optional[str] = None
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_kegiatan(
-    kegiatan: KegiatanCreateRequest, db: db_dependency, user: user_dependency
+    kegiatan: Json[KegiatanCreateRequest],
+    db: db_dependency,
+    user: user_dependency,
+    file: Union[UploadFile, str] = File(None),
 ):
     if user["is_admin"]:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Admin cannot create kegiatan",
         )
-
-    if kegiatan.gambar is not None:
-        path = f"assets/uploads/{kegiatan.gambar}"
-        kegiatan.gambar = path
 
     new_kegiatan = Kegiatan(
         nama_kegiatan=kegiatan.nama_kegiatan,
@@ -38,6 +36,15 @@ async def create_kegiatan(
         deskripsi=kegiatan.deskripsi,
         user_id=user["id"],
     )
+
+    uploads_path = "app/uploads/"
+
+    if file:
+        image = await file.read()
+        with open(f"{uploads_path}{file.filename}", "wb") as dump:
+            dump.write(image)
+        new_kegiatan.gambar = f"{uploads_path}{file.filename}"
+
     db.add(new_kegiatan)
     db.commit()
     db.refresh(new_kegiatan)
@@ -57,14 +64,35 @@ async def get_kegiatan(db: db_dependency, user: user_dependency):
     return kegiatan
 
 
+@router.get("/{kegiatan_id}")
+async def get_kegiatan_by_id(
+    kegiatan_id: int, db: db_dependency, user: user_dependency
+):
+    kegiatan = db.query(Kegiatan).filter(Kegiatan.id == kegiatan_id).first()
+    if user["id"] != kegiatan.user_id and not user["is_admin"]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to access this kegiatan",
+        )
+
+    if not kegiatan:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Kegiatan not found"
+        )
+
+    return kegiatan
+
+
 @router.put("/{kegiatan_id}")
 async def update_kegiatan(
     kegiatan_id: int,
-    kegiatan: KegiatanCreateRequest,
+    kegiatan: Json[KegiatanCreateRequest],
     db: db_dependency,
     user: user_dependency,
+    file: Union[UploadFile, str] = File(None),
 ):
     kegiatan_data = db.query(Kegiatan).filter(Kegiatan.id == kegiatan_id).first()
+
     if not kegiatan_data:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Kegiatan not found"
@@ -76,15 +104,16 @@ async def update_kegiatan(
             detail="You are not allowed to update this kegiatan",
         )
 
-    if kegiatan.gambar is not None:
-        path = f"assets/uploads/{kegiatan.gambar}"
-        kegiatan.gambar = path
-
     kegiatan_data.nama_kegiatan = kegiatan.nama_kegiatan
     kegiatan_data.tanggal = kegiatan.tanggal
     kegiatan_data.tempat = kegiatan.tempat
     kegiatan_data.deskripsi = kegiatan.deskripsi
-    kegiatan_data.gambar = kegiatan.gambar
+
+    if file:
+        image = await file.read()
+        with open(f"app/uploads/{file.filename}", "wb") as dump:
+            dump.write(image)
+        kegiatan_data.gambar = file.filename
 
     db.commit()
     db.refresh(kegiatan_data)
