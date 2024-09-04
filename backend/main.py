00c1d639a79bb5580157b3eb734d8bf1c2c8caa6
taufiqdp <topexdw@gmail.com>
2024-09-04 15:33:55 +0700

    update

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import auth, kegiatan, users, pamong
+from app.routers import auth, kegiatan, users, pamong, agenda
 from app.internal import admin
 from app.database import Base, engine
 
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 def health_check():
-    return {"status": "ok", "last_updated": "2024-09-04T09:06:00"}
+    return {"status": "ok", "last_updated": "2024-09-04T15:36:00"}
 
 
 app.include_router(auth.router)
@@ -31,6 +31,7 @@ app.include_router(users.router)
 app.include_router(kegiatan.router)
 app.include_router(admin.router)
 app.include_router(pamong.router)
+app.include_router(agenda.router)
 
 
 # Delete later
