7efc99d32e252c1a09dbe565a07cfc4b9ab23f04
taufiqdp <topexdw@gmail.com>
2024-09-14 10:15:07 +0700

    add tambah pamong page

--- app/pamong/tambah-pamong/page.js
+++ app/pamong/tambah-pamong/page.js
@@ -2,5 +2,5 @@ import { pamongInputAction } from "@/actions/pamong-input-action";
 import PamongInputForm from "@/components/pamong-input-form";
 
 export default function TambahPamongPage() {
-  return <PamongInputForm pamongInputAction={pamongInputAction} />;
+  return <PamongInputForm pamongAction={pamongInputAction} />;
 }
