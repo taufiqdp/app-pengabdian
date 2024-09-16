a98d8ec40e6cfd660579fa2726ccc994f921398a
taufiqdp <topexdw@gmail.com>
2024-09-16 09:44:09 +0700

    update: add date sorting

--- frontend/components/table-kegiatan.jsx
+++ frontend/components/table-kegiatan.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -10,26 +10,44 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, ArrowUpDown } from "lucide-react";
 import Link from "next/link";
 
 export default function TableKegiatan({ dataKegiatan }) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortOrder, setSortOrder] = useState("asc");
   const itemsPerPage = 10;
+
+  const sortedData = [...dataKegiatan].sort((a, b) => {
+    const dateA = new Date(a.tanggal);
+    const dateB = new Date(b.tanggal);
+    return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
+  });
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentActivities = dataKegiatan.slice(
-    indexOfFirstItem,
-    indexOfLastItem
-  );
-  const totalPages = Math.ceil(dataKegiatan.length / itemsPerPage);
+  const currentActivities = sortedData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(sortedData.length / itemsPerPage);
+
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    setCurrentPage(1);
+  };
+
   return (
     <>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-1/6">Tanggal</TableHead>
+            <TableHead className="w-1/6">
+              <button
+                onClick={toggleSortOrder}
+                className="h-8 w-full flex flex-row items-center justify-between"
+              >
+                <p>Tanggal</p>
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+              </button>
+            </TableHead>
             <TableHead className="w-2/6">Nama Kegiatan</TableHead>
             <TableHead className="w-1/4">Tempat</TableHead>
             <TableHead className="w-1/4">Nama Pamong</TableHead>
@@ -64,8 +82,8 @@ export default function TableKegiatan({ dataKegiatan }) {
       <div className="flex justify-between items-center">
         <div className="text-gray-700 font-light">
           Menampilkan {indexOfFirstItem + 1} sampai{" "}
-          {Math.min(indexOfLastItem, dataKegiatan.length)} dari{" "}
-          {dataKegiatan.length} kegiatan
+          {Math.min(indexOfLastItem, sortedData.length)} dari{" "}
+          {sortedData.length} kegiatan
         </div>
         <div className="flex items-center justify-end space-x-2 min-w-36">
           <Button
