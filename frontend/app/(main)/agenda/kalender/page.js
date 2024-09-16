3a606a0d8e9c08ac8064f866458c287f63ebfdd4
taufiqdp <topexdw@gmail.com>
2024-09-16 10:27:34 +0700

    update calendar page

--- frontend/app/(main)/agenda/kalender/page.js
+++ frontend/app/(main)/agenda/kalender/page.js
@@ -1,11 +1,15 @@
+import { agedaInputAction } from "@/actions/agenda-input-action";
 import { BigCalendar } from "@/components/big-calendar";
+import { getAgendaThisMonth } from "@/lib/agenda";
 
-export default function KalenderPage() {
+export default async function KalenderPage() {
+  const agendaData = await getAgendaThisMonth();
+
   return (
     <>
       <h1 className="text-2xl font-bold pb-4">Kalender</h1>
       <div className="bg-white p-6 rounded-md shadow-md">
-        <BigCalendar />
+        <BigCalendar agendaAction={agedaInputAction} agendaData={agendaData} />
       </div>
     </>
   );
