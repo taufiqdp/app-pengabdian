a23bb020e40e9b651f224a8646755ce42381c956
taufiqdp <topexdw@gmail.com>
2024-09-19 10:02:28 +0700

    update: image size

--- frontend/components/kegiatan-detail.jsx
+++ frontend/components/kegiatan-detail.jsx
@@ -16,47 +16,49 @@ export async function KegiatanDetailComponent({ idKegiatan }) {
   }
 
   return (
-    <div className="min-h-screen bg-background rounded-md shadow-md">
-      <div className="relative h-[40vh] md:h-[40vh] lg:h-[50vh]">
-        <Image
-          src={imageKegiatan}
-          alt={dataKegiatan.nama_kegiatan}
-          fill
-          priority
-          className="rounded-t-md"
-        />
-      </div>
-
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="flex flex-wrap gap-6 mb-8">
-          <div className="flex items-center text-muted-foreground">
-            <CalendarIcon className="w-5 h-5 mr-2" />
-            <span>
-              {format(new Date(dataKegiatan.tanggal), "EEEE, d MMMM yyyy", {
-                locale: id,
-              })}
-            </span>
-          </div>
-          <div className="flex items-center text-muted-foreground">
-            <MapPinIcon className="w-5 h-5 mr-2" />
-            <span>{dataKegiatan.tempat}</span>
-          </div>
-          <Link
-            href={`/pamong/${dataKegiatan.pamong_id}`}
-            className="flex items-center text-muted-foreground hover:text-black hover:underline"
-          >
-            <UserIcon className="w-5 h-5 mr-2" />
-            <span>{dataKegiatan.nama_pamong}</span>
-          </Link>
+    <div className="bg-background rounded-md shadow-md">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="relative h-[40vh] md:h-[40vh] w-[40hv]">
+          <Image
+            src={imageKegiatan}
+            alt={dataKegiatan.nama_kegiatan}
+            fill
+            priority
+            className="h-full w-full object-cover rounded-md"
+          />
         </div>
 
-        <h2 className="text-2xl font-semibold mb-4">
-          {dataKegiatan.nama_kegiatan}
-        </h2>
-        <div className="prose prose-lg max-w-none">
-          <p className="text-muted-foreground whitespace-pre-wrap">
-            {dataKegiatan.deskripsi}
-          </p>
+        <div>
+          <div className="flex flex-wrap gap-6 mb-8">
+            <div className="flex items-center text-muted-foreground">
+              <CalendarIcon className="w-5 h-5 mr-2" />
+              <span>
+                {format(new Date(dataKegiatan.tanggal), "EEEE, d MMMM yyyy", {
+                  locale: id,
+                })}
+              </span>
+            </div>
+            <div className="flex items-center text-muted-foreground">
+              <MapPinIcon className="w-5 h-5 mr-2" />
+              <span>{dataKegiatan.tempat}</span>
+            </div>
+            <Link
+              href={`/pamong/${dataKegiatan.pamong_id}`}
+              className="flex items-center text-muted-foreground hover:text-black hover:underline"
+            >
+              <UserIcon className="w-5 h-5 mr-2" />
+              <span>{dataKegiatan.nama_pamong}</span>
+            </Link>
+          </div>
+
+          <h2 className="text-2xl font-semibold mb-4">
+            {dataKegiatan.nama_kegiatan}
+          </h2>
+          <div className="prose prose-lg max-w-none">
+            <p className="text-muted-foreground whitespace-pre-wrap">
+              {dataKegiatan.deskripsi}
+            </p>
+          </div>
         </div>
       </div>
     </div>
